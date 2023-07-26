da5b72db0bfc63bb2ed21cb2a8dbdfeafd9ca51e
Nick Topacio <ntopacio25@gmail.com>
2023-07-26 17:22:29 -0400

    Start weather card

--- src/main.js
+++ src/main.js
@@ -1,4 +1,23 @@
 import "../styles/modern-normalize.css";
 import "../styles/style.css";
 // import component files here
+import "../styles/components/weather.css";
 import "../styles/utils.css";
+
+function loadCurrTime() {
+  const currDate = new Date();
+  let suffix = "AM";
+  let hour = currDate.getHours();
+  if (hour > 12) {
+    hour = hour - 12;
+    suffix = "PM";
+  }
+  let min = currDate.getMinutes();
+  if (min < 10) {
+    min = "0" + min;
+  }
+  let time = document.getElementById("weather__time-amt");
+  time.textContent = hour + ":" + min + " " + suffix;
+}
+
+loadCurrTime();
